f57074bf4a310188d4b027daf23e18ba91ca4552
tjpuzhaolei <tjpuzhaolei@163.com>
2018-07-30 14:29:49 +0800

    update

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 from __future__ import division
 import cv2
 import mxnet as mx
@@ -5,7 +6,7 @@ import numpy as np
 from mxnet import nd, gluon
 import threading
 import xml.etree.ElementTree as ET
-
+import pdb
 
 def try_gpu(num_list):
     """If GPU is available, return mx.gpu(0); else return mx.cpu()"""
@@ -26,8 +27,8 @@ def bbox_iou(box1, box2, transform=True):
     """
     Returns the IoU of two bounding boxes
     """
-    box1 = nd.abs(box1)
-    box2 = nd.abs(box2)
+    box1 = np.abs(box1)
+    box2 = np.abs(box2)
     if transform:
         tmp_box1 = box1.copy()
         tmp_box1[:, 0] = box1[:, 0] - box1[:, 2] / 2.0
@@ -46,14 +47,15 @@ def bbox_iou(box1, box2, transform=True):
     b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
 
     # get the corrdinates of the intersection rectangle
-    inter_rect_x1 = nd.where(b1_x1 > b2_x1, b1_x1, b2_x1)
-    inter_rect_y1 = nd.where(b1_y1 > b2_y1, b1_y1, b2_y1)
-    inter_rect_x2 = nd.where(b1_x2 < b2_x2, b1_x2, b2_x2)
-    inter_rect_y2 = nd.where(b1_y2 < b2_y2, b1_y2, b2_y2)
+    inter_rect_x1 = np.where(b1_x1 > b2_x1, b1_x1, b2_x1)
+    inter_rect_y1 = np.where(b1_y1 > b2_y1, b1_y1, b2_y1)
+    inter_rect_x2 = np.where(b1_x2 < b2_x2, b1_x2, b2_x2)
+    inter_rect_y2 = np.where(b1_y2 < b2_y2, b1_y2, b2_y2)
 
     # Intersection area
-    inter_area = nd.clip(inter_rect_x2 - inter_rect_x1 + 1, a_min=0, a_max=10000) * nd.clip(
-        inter_rect_y2 - inter_rect_y1 + 1, a_min=0, a_max=10000)
+    #inter_area = np.clip(inter_rect_x2 - inter_rect_x1 + 1, a_min=0, a_max=10000) * np.clip(
+    #    inter_rect_y2 - inter_rect_y1 + 1, a_min=0, a_max=10000)
+    inter_area = (inter_rect_x2 - inter_rect_x1 + 1) * (inter_rect_y2 - inter_rect_y1 + 1)
 
     # Union Area
     b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
@@ -61,10 +63,12 @@ def bbox_iou(box1, box2, transform=True):
     iou = inter_area / (b1_area + b2_area - inter_area)
     # iou[inter_area >= b1_area] = 0.8
     # iou[inter_area >= b2_area] = 0.8
-    return nd.clip(iou, 1e-5, 1. - 1e-5)
+    #return np.clip(iou, 1e-5, 1. - 1e-5)
+    return iou
 
 
 def predict_transform(prediction, input_dim, anchors):
+
     ctx = prediction.context
     if not isinstance(anchors, nd.NDArray):
         anchors = nd.array(anchors, ctx=ctx)
@@ -74,6 +78,7 @@ def predict_transform(prediction, input_dim, anchors):
     strides = [13, 26, 52]
     step = [(0, 507), (507, 2535), (2535, 10647)]
     for i in range(3):
+	#pdb.set_trace()
         stride = strides[i]
         grid = np.arange(stride)
         a, b = np.meshgrid(grid, grid)
@@ -107,14 +112,22 @@ def predict_transform(prediction, input_dim, anchors):
         prediction[:, step[i][0]:step[i][1], :2] += x_y_offset
         prediction[:, step[i][0]:step[i][1], :2] *= (float(input_dim) / stride)
         prediction[:, step[i][0]:step[i][1], 2:4] = \
-            nd.exp(nd.clip(prediction[:, step[i][0]:step[i][1], 2:4], a_min=0., a_max=10)) * tmp_anchors
+            nd.exp(prediction[:, step[i][0]:step[i][1], 2:4]) * tmp_anchors
+        #prediction[:, step[i][0]:step[i][1], 2:4] /= float(input_dim) 
+            #nd.exp(nd.clip(prediction[:, step[i][0]:step[i][1], 2:4], a_min=0., a_max=10)) * tmp_anchors
 
     return prediction
 
 
 def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
-    conf_mask = (prediction[:, :, 4] > confidence).expand_dims(2)
-    prediction = prediction * conf_mask
+
+    ### 
+    box_confidence = nd.repeat(nd.expand_dims(prediction[:,:,4],2),num_classes,axis=2)
+    prediction[:,:,5:5+num_classes] = prediction[:,:,5:5+num_classes] * box_confidence
+    ###
+    #conf_mask = (prediction[:, :, 4] > confidence).expand_dims(2)
+    conf_mask = prediction[:, :, 5:5+num_classes] > confidence
+    prediction[:, :, 5:5+num_classes]  = prediction[:, :, 5:5+num_classes] * conf_mask
 
     batch_size = prediction.shape[0]
 
@@ -124,7 +137,7 @@ def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
     box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
     box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
     prediction[:, :, :4] = box_corner[:, :, :4]
-
+    #pdb.set_trace()
     output = None
 
     for ind in range(batch_size):
@@ -135,7 +148,8 @@ def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
         max_conf = max_conf.astype("float32").expand_dims(1)
         max_conf_score = max_conf_score.astype("float32").expand_dims(1)
         image_pred = nd.concat(image_pred[:, :5], max_conf, max_conf_score, dim=1).asnumpy()
-        non_zero_ind = np.nonzero(image_pred[:, 4])
+        non_zero_ind = np.nonzero(image_pred[:, 5])
+	
         try:
             image_pred_ = image_pred[non_zero_ind, :].reshape((-1, 7))
         except Exception as e:
@@ -146,7 +160,8 @@ def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
         # Get the various classes detected in the image
         img_classes = np.unique(image_pred_[:, -1])
         # -1 index holds the class index
-
+	
+		
         for cls in img_classes:
             # get the detections with one particular class
             cls_mask = image_pred_ * np.expand_dims(image_pred_[:, -1] == cls, axis=1)
@@ -155,10 +170,11 @@ def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
 
             # sort the detections such that the entry with the maximum objectness
             # confidence is at the top
-            conf_sort_index = np.argsort(image_pred_class[:, 4])[::-1]
+            conf_sort_index = np.argsort(image_pred_class[:, 5])[::-1]
             image_pred_class = image_pred_class[conf_sort_index]
             idx = image_pred_class.shape[0]
-
+	    
+	    pdb_num = 0
             for i in range(idx):
                 # Get the IOUs of all boxes that come after the one we are looking at
                 # in the loop
@@ -166,7 +182,7 @@ def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
                     box1 = np.expand_dims(image_pred_class[i], 0)
                     box2 = image_pred_class[i + 1:]
                     box1 = np.repeat(box1, repeats=box2.shape[0], axis=0)
-                    ious = bbox_iou(box1, box2, transform=False).asnumpy()
+                    ious = bbox_iou(box1, box2, transform=False)
                 except ValueError:
                     break
                 except IndexError:
@@ -177,8 +193,9 @@ def write_results(prediction, num_classes, confidence=0.5, nms_conf=0.4):
                 image_pred_class[i + 1:] *= iou_mask
 
                 # Remove the non-zero entries
-                non_zero_ind = np.nonzero(image_pred_class[:, 4])
+                non_zero_ind = np.nonzero(image_pred_class[:, 5])
                 image_pred_class = image_pred_class[non_zero_ind].reshape((-1, 7))
+		pdb_num +=1
 
             batch_ind = np.ones((image_pred_class.shape[0], 1)) * ind
 
